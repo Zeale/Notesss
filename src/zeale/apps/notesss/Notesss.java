0ab5bc29b1491dbb3197eb5e2161a76684ce8631
Zeale <zeale@dusttoash.org>
2019-01-16 10:01:09 -0600

    Added a window

---
+++ src/zeale/apps/notesss/Notesss.java
@@ -0,0 +1,15 @@
+package zeale.apps.notesss;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Notesss extends Application {
+	public static void main(String[] args) {
+		Application.launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.show();
+	}
+}
