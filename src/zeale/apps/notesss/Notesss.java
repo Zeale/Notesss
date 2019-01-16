498cdb620d62831005afbc9fa6fba98d77248440
Zeale <zeale@dusttoash.org>
2019-01-16 10:53:36 -0600

    Added a text box

--- src/zeale/apps/notesss/Notesss.java
+++ src/zeale/apps/notesss/Notesss.java
@@ -1,15 +1,25 @@
 package zeale.apps.notesss;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
 import javafx.stage.Stage;
 
 public class Notesss extends Application {
+
+	private final TextArea area = new TextArea();
+	private final Scene scene = new Scene(area);
+	{
+		area.setPromptText("Type something here...");
+	}
+
 	public static void main(String[] args) {
 		Application.launch(args);
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setScene(scene);
 		primaryStage.show();
 	}
 }
