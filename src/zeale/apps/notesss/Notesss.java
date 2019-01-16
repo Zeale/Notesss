9923e984b23a8dd767fe620d10e36d144203e925
Zeale <zeale@dusttoash.org>
2019-01-16 11:19:32 -0600

    Added a menu bar

--- src/zeale/apps/notesss/Notesss.java
+++ src/zeale/apps/notesss/Notesss.java
@@ -2,16 +2,25 @@ package zeale.apps.notesss;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class Notesss extends Application {
+	private final Menu save = new Menu("Save");
+	private final MenuBar menubar = new MenuBar(save);
 
 	private final TextArea area = new TextArea();
-	private final Scene scene = new Scene(area);
 	{
 		area.setPromptText("Type something here...");
 	}
+	private final BorderPane pane = new BorderPane(area);
+	{
+		pane.setTop(menubar);
+	}
+	private final Scene scene = new Scene(pane);
 
 	public static void main(String[] args) {
 		Application.launch(args);
