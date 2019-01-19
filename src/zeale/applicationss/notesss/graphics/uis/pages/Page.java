8cb7cdbcc20fde27e4e107a0212859248d3ea1af
Zeale <zeale@dusttoash.org>
2019-01-19 00:39:13 -0600

    Added a superclass for normal windows

---
+++ src/zeale/applicationss/notesss/graphics/uis/pages/Page.java
@@ -0,0 +1,18 @@
+package zeale.applicationss.notesss.graphics.uis.pages;
+
+import javafx.stage.Stage;
+
+public interface Page {
+
+	default Stage display() {
+		return display(new Stage());
+	}
+
+	/**
+	 * Displays this {@link Page} on the given {@link Stage} and returns the
+	 * {@link Stage} when finished.
+	 * 
+	 * @return The {@link Stage} that this {@link Page} was displayed on.
+	 */
+	Stage display(Stage stage);
+}
