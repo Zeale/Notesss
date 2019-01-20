0b90ab2def3fb4cdff41c9c997c408fea0b94885
Zeale <zeale@dusttoash.org>
2019-01-20 00:04:58 -0600

    Added scene roots to homepage

--- src/zeale/applicationss/notesss/graphics/uis/pages/HomePage.java
+++ src/zeale/applicationss/notesss/graphics/uis/pages/HomePage.java
@@ -7,6 +7,11 @@ import javafx.stage.Stage;
 
 // Fullscreen will have a side menu, window mode will have a regular menu bar.
 public class HomePage implements Page {
+
+	private final AnchorPane root = new AnchorPane();
+	private final BorderPane wrapper = new BorderPane(root);
+	private final Scene scene = new Scene(wrapper);
+
 	@Override
 	public Stage display(Stage stage) {
 		stage.setScene(scene);
