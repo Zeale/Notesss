3ffb6c6e88bf87aad75dc0791c9df8e0fe8b6685
Zeale <zeale@dusttoash.org>
2019-01-21 00:57:30 -0600

    Added screen scaling functions to Utilities

--- src/zeale/applicationss/notesss/utilities/Utilities.java
+++ src/zeale/applicationss/notesss/utilities/Utilities.java
@@ -22,4 +22,36 @@ public final class Utilities {
 			Platform.runLater(task);
 	}
 
+	public Utilities(double anchorWidth, double anchorHeight) {
+		this.anchorWidth = anchorWidth;
+		this.anchorHeight = anchorHeight;
+	}
+
+	private final double anchorWidth, anchorHeight;
+
+	public static void scaleToWindow(Node node, double anchorWidth, double anchorHeight) {
+		scaleHeight(node, anchorHeight);
+		scaleWidth(node, anchorWidth);
+	}
+
+	public static void scaleWidth(Node node, double anchorWidth) {
+		node.setScaleX(node.getScene().getWindow().getWidth() / anchorWidth);
+	}
+
+	public static void scaleHeight(Node node, double anchorHeight) {
+		node.setScaleY(node.getScene().getWindow().getHeight() / anchorHeight);
+	}
+
+	public void scaleWidth(Node node) {
+		scaleWidth(node, anchorWidth);
+	}
+
+	public void scaleHeight(Node node) {
+		scaleHeight(node, anchorHeight);
+	}
+
+	public void scaleToWindow(Node node) {
+		scaleToWindow(node, anchorWidth, anchorHeight);
+	}
+
 }
