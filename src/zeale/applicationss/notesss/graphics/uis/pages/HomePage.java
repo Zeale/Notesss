a018f3614be7083a9d52f3d73e8987807de3c702
Zeale <zeale@dusttoash.org>
2019-01-21 23:24:35 -0600

    Added a few elements to home page

--- src/zeale/applicationss/notesss/graphics/uis/pages/HomePage.java
+++ src/zeale/applicationss/notesss/graphics/uis/pages/HomePage.java
@@ -3,12 +3,27 @@ package zeale.applicationss.notesss.graphics.uis.pages;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 // Fullscreen will have a side menu, window mode will have a regular menu bar.
 public class HomePage implements Page {
 
+	private final Text title = new Text("Notesss");
+
 	private final AnchorPane root = new AnchorPane();
+	{
+		StackPane wrapper = new StackPane(title);
+		root.getChildren().add(wrapper);
+		title.setFont(Font.font(72));
+		title.setFill(Color.LIGHTGRAY);
+		AnchorPane.setLeftAnchor(wrapper, 0d);
+		AnchorPane.setRightAnchor(wrapper, 0d);
+		AnchorPane.setTopAnchor(wrapper, 25d);
+	}
 	private final BorderPane wrapper = new BorderPane(root);
 	private final Scene scene = new Scene(wrapper);
 
@@ -17,4 +32,5 @@ public class HomePage implements Page {
 		stage.setScene(scene);
 		return stage;
 	}
+
 }
