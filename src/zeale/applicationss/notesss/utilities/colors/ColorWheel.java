3b7df4f979ef591344e785417e31da42313f423d
Zeale <zeale@dusttoash.org>
2019-01-21 23:10:59 -0600

    Updated ColorWheel class to use arrays

--- src/zeale/applicationss/notesss/utilities/colors/ColorWheel.java
+++ src/zeale/applicationss/notesss/utilities/colors/ColorWheel.java
@@ -1,30 +1,27 @@
 package zeale.applicationss.notesss.utilities.colors;
 
-import java.util.LinkedList;
-import java.util.List;
-
-import org.alixia.chatroom.api.QuickList;
+import java.util.Arrays;
 
 import javafx.scene.paint.Paint;
 import zeale.applicationss.notesss.utilities.generators.Generator;
 
 public class ColorWheel implements Generator<Paint> {
 
-	private final List<Paint> colors;
+	private final Paint[] colors;
 
-	public ColorWheel(List<Paint> colors) {
-		this.colors = new LinkedList<>(colors);
+	public Paint[] getColors() {
+		return Arrays.copyOf(colors, colors.length);
 	}
 
 	public ColorWheel(Paint... colors) {
-		this.colors = new QuickList<>(colors);
+		this.colors = Arrays.copyOf(colors, colors.length);
 	}
 
 	private int pos;
 
 	@Override
 	public Paint next() {
-		return colors.get(++pos >= colors.size() ? (pos = 0) : pos);
+		return colors[++pos >= colors.length ? (pos = 0) : pos];
 	}
 
 }
