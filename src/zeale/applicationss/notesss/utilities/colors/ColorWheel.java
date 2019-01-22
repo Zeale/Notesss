245de77b0435e43ead9f1099f5c02dca88ee78b2
Zeale <zeale@dusttoash.org>
2019-01-21 23:37:17 -0600

    Added methods to ColorWheel

--- src/zeale/applicationss/notesss/utilities/colors/ColorWheel.java
+++ src/zeale/applicationss/notesss/utilities/colors/ColorWheel.java
@@ -3,14 +3,19 @@ package zeale.applicationss.notesss.utilities.colors;
 import java.util.Arrays;
 
 import javafx.scene.paint.Paint;
+import zeale.applicationss.notesss.utilities.Lengthed;
 import zeale.applicationss.notesss.utilities.generators.Generator;
 
-public class ColorWheel implements Generator<Paint> {
+public class ColorWheel implements Generator<Paint>, Lengthed {
 
 	private final Paint[] colors;
 
 	public Paint[] getColors() {
 		return Arrays.copyOf(colors, colors.length);
+	}
+
+	public Paint getColor(int index) {
+		return colors[index % len()];
 	}
 
 	public ColorWheel(Paint... colors) {
@@ -22,6 +27,11 @@ public class ColorWheel implements Generator<Paint> {
 	@Override
 	public Paint next() {
 		return colors[++pos >= colors.length ? (pos = 0) : pos];
+	}
+
+	@Override
+	public int length() {
+		return colors.length;
 	}
 
 }
