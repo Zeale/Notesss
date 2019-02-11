11605ccae4462b399dc56b81f48476f2d15f76f6
Zeale <zeale@dusttoash.org>
2019-02-11 07:52:49 -0600

    HomePage appears correctly(ish)

--- src/zeale/applicationss/notesss/graphics/uis/pages/AdvancedHomePage.java
+++ src/zeale/applicationss/notesss/graphics/uis/pages/AdvancedHomePage.java
@@ -1,34 +1,77 @@
 package zeale.applicationss.notesss.graphics.uis.pages;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import zeale.applicationss.notesss.ApplicationProperties;
 import zeale.applicationss.notesss.Notesss;
 import zeale.applicationss.notesss.utilities.Utilities;
-import zeale.apps.tools.console.std.StandardConsole.EmbeddedStandardConsoleView;
 
 public class AdvancedHomePage implements Page {
 
-	private final EmbeddedStandardConsoleView view = Notesss.CONSOLE.getEmbeddedView();
+	private final static StackPane getItemPaneForLayout(Pos position, Node... children) {
+		StackPane pane = new StackPane(children);
+		pane.setAlignment(position);
+		return pane;
+	}
+
+	private final Pane viewDummy = new Pane(), tabsDummy = new Pane(), historyDummy = new Pane(),
+			settingsDummy = new Pane();
 
 	private final Text title = new Text("Notesss");
 	private final TextField searchBar = new TextField();
 
 	private final VBox titleBox = new VBox(20, title, searchBar);
-	private final TilePane flow = new TilePane(view);
-	private final VBox root = new VBox(75, titleBox, flow);
+	private final VBox leftItemWrapping = new VBox(30, getItemPaneForLayout(Pos.TOP_LEFT, viewDummy),
+			getItemPaneForLayout(Pos.BOTTOM_LEFT, tabsDummy)),
+			rightItemWrapping = new VBox(25, getItemPaneForLayout(Pos.TOP_RIGHT, historyDummy),
+					getItemPaneForLayout(Pos.BOTTOM_RIGHT, settingsDummy));
+	private final HBox itemsWrapping = new HBox(250, leftItemWrapping, rightItemWrapping);
+
+	private final VBox root = new VBox(75, titleBox, itemsWrapping);
 	private final ScrollPane scrollWrapper = new ScrollPane(root);
 	private final Scene scene = new Scene(scrollWrapper);
 
 	{
+		// DUMMY
+		viewDummy.setPrefSize(400, 600);
+		tabsDummy.setPrefSize(400, 150);
+		historyDummy.setPrefSize(300, 300);
+		settingsDummy.setPrefSize(300, 450);
+		viewDummy.setMinSize(400, 600);
+		tabsDummy.setMinSize(400, 150);
+		historyDummy.setMinSize(300, 300);
+		settingsDummy.setMinSize(300, 450);
+
+		viewDummy.setBackground(Utilities.getBackgroundFromColor(Color.GREEN));
+		tabsDummy.setBackground(Utilities.getBackgroundFromColor(Color.PINK));
+		historyDummy.setBackground(Utilities.getBackgroundFromColor(Color.FIREBRICK));
+		settingsDummy.setBackground(Utilities.getBackgroundFromColor(Color.AQUAMARINE));
+
+		// Shift left column down a little.
+		HBox.setMargin(leftItemWrapping, new Insets(20, 0, 0, 0));
+
 		root.setBackground(Utilities.getBackgroundFromColor(Notesss.getNextColor()));
-		view.setBackground(Utilities.getBackgroundFromColor(Notesss.getNextColor()));
-		Utilities.setAllAnchors(25d, view);
+		Utilities.setAllAnchors(0d, root);
+		titleBox.setFillWidth(false);
+		titleBox.setAlignment(Pos.CENTER);
+		root.setFillWidth(false);
+		root.setAlignment(Pos.CENTER);
+
+		scrollWrapper.setFitToHeight(true);
+		scrollWrapper.setFitToWidth(true);
+		root.setPadding(new Insets(50));
+
 	}
 
 	@Override
