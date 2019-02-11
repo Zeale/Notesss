43a827da6c3223c53a8ce88e0eb89d7aec038f48
Zeale <zeale@dusttoash.org>
2019-02-11 08:28:09 -0600

    ColorWheel now has foreground and background colors

--- src/zeale/applicationss/notesss/utilities/colors/ColorWheel.java
+++ src/zeale/applicationss/notesss/utilities/colors/ColorWheel.java
@@ -8,36 +8,66 @@ import zeale.applicationss.notesss.utilities.generators.Generator;
 
 public class ColorWheel implements Generator<Paint>, Lengthed {
 
-	private final Paint[] colors;
+	// TODO Make ColorWheel generic (so instances that only hold Color objects can
+	// return Color arrays instead of Paint arrays).
+
+	private final Paint[] foregrounds, backgrounds;
+
+	public Paint[] getForegrounds() {
+		return Arrays.copyOf(foregrounds, foregrounds.length);
+	}
+
+	public Paint getForegroundColor(int index) {
+		return foregrounds[index % foregrounds.length];
+	}
+
+	public Paint getBackgroundColor(int index) {
+		return backgrounds[index % backgrounds.length];
+	}
+
+	public Paint getf(int index) {
+		return getForegroundColor(index);
+	}
+
+	public Paint getb(int index) {
+		return getBackgroundColor(index);
+	}
+
+	public Paint[] getBackgrounds() {
+		return Arrays.copyOf(backgrounds, backgrounds.length);
+	}
 
 	public Paint[] getColors() {
-		return Arrays.copyOf(colors, colors.length);
+		Paint[] colors = Arrays.copyOf(foregrounds, len());
+		System.arraycopy(backgrounds, 0, foregrounds, foregrounds.length, backgrounds.length);
+		return colors;
 	}
 
 	public Paint getColor(int index) {
-		return colors[index % len()];
+		return (index %= (len())) > foregrounds.length ? backgrounds[index -= foregrounds.length] : foregrounds[index];
 	}
 
-	public ColorWheel(Paint... colors) {
-		this.colors = Arrays.copyOf(colors, colors.length);
+	public ColorWheel(Paint[] foregrounds, Paint... backgrounds) {
+		this.foregrounds = Arrays.copyOf(foregrounds, foregrounds.length);
+		this.backgrounds = Arrays.copyOf(backgrounds, backgrounds.length);
 	}
 
 	private int pos;
 
 	public Paint startOver() {
-		Paint color = colors[pos];
+		Paint color = getColor(pos);
 		pos = 0;
 		return color;
 	}
 
 	@Override
 	public Paint next() {
-		return colors[++pos >= colors.length ? (pos = 0) : pos];
+		return getColor(++pos >= len() ? (pos = 0) : pos);
 	}
 
 	@Override
 	public int length() {
-		return colors.length;
+		return foregrounds.length + backgrounds.length;
 	}
 
 }
