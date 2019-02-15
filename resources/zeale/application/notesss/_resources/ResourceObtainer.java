b7aac00e23dd0485af1de1de08a28d038a268651
Zeale <zeale@dusttoash.org>
2019-02-15 17:03:10 -0600

    Added some functions to ResourceObtainer

--- resources/zeale/application/notesss/_resources/ResourceObtainer.java
+++ resources/zeale/application/notesss/_resources/ResourceObtainer.java
@@ -1,6 +1,9 @@
 package zeale.application.notesss._resources;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
 public class ResourceObtainer {
 	/**
@@ -18,4 +21,33 @@ public class ResourceObtainer {
 		return ResourceObtainer.class
 				.getResourceAsStream(path.startsWith("int:") && path.length() > 4 ? path.substring(4) : path);
 	}
+
+	public static String string(String path, byte[] arr, Charset encoding) {
+		StringBuilder builder = new StringBuilder();
+		int amount;
+		try (InputStream in = resource(path)) {
+			while ((amount = in.read(arr)) != -1)
+				builder.append(new String(arr, 0, amount, encoding));
+		} catch (IOException e) {
+			return null;
+		}
+		return builder.toString();
+	}
+
+	public static String string(String path, byte[] arr) {
+		return string(path, arr, StandardCharsets.US_ASCII);
+	}
+
+	public static String string(String path, int bufferSize) {
+		return string(path, new byte[bufferSize]);
+	}
+
+	public static String string(String path, int bufferSize, Charset charset) {
+		return string(path, new byte[bufferSize], charset);
+	}
+
+	public static String string(String path) {
+		return string(path, 2048);
+	}
+
 }
