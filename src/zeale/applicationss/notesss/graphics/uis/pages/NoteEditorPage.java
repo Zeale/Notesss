479f2c6734cf0c97b48318ef766fa9fd141187d7
Zeale <zeale@dusttoash.org>
2019-02-16 13:20:43 -0600

    Added note editor page

---
+++ src/zeale/applicationss/notesss/graphics/uis/pages/NoteEditorPage.java
@@ -0,0 +1,14 @@
+package zeale.applicationss.notesss.graphics.uis.pages;
+
+import javafx.stage.Stage;
+import zeale.applicationss.notesss.ApplicationProperties;
+
+public class NoteEditorPage implements Page {
+
+	@Override
+	public Stage display(Stage stage, ApplicationProperties properties) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
