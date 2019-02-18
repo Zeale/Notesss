eefb66ff2a156d0c14c34b4bc5f020e4a6a18e7a
Zeale <zeale@dusttoash.org>
2019-02-18 08:53:19 -0600

    Updated Note Editor

--- src/zeale/applicationss/notesss/graphics/uis/pages/NoteEditorPage.java
+++ src/zeale/applicationss/notesss/graphics/uis/pages/NoteEditorPage.java
@@ -1,14 +1,24 @@
 package zeale.applicationss.notesss.graphics.uis.pages;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import zeale.applicationss.notesss.ApplicationProperties;
 
 public class NoteEditorPage implements Page {
 
+	private final Menu fileMenu = new Menu("File");
+	private final MenuBar menubar = new MenuBar(fileMenu);
+	private final BorderPane root = new BorderPane();
+	{	root.setTop(menubar);	}
+	private final Scene scene = new Scene(root);
+
 	@Override
 	public Stage display(Stage stage, ApplicationProperties properties) {
-		// TODO Auto-generated method stub
-		return null;
+		stage.setScene(scene);
+		return stage;
 	}
 
 }
