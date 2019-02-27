260b0359baceb1578e0450a8e34e340aa42fdc45
Zeale <zeale@dusttoash.org>
2019-02-27 12:58:15 -0600

    Added nodes to NoteManagementPage

--- src/zeale/applicationss/notesss/graphics/uis/pages/NoteManagementPage.java
+++ src/zeale/applicationss/notesss/graphics/uis/pages/NoteManagementPage.java
@@ -1,14 +1,30 @@
 package zeale.applicationss.notesss.graphics.uis.pages;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Accordion;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import zeale.applicationss.notesss.ApplicationProperties;
 
 public class NoteManagementPage implements Page {
 
+	
+	
+	private final BorderPane leftMenu = new BorderPane();
+	private final Accordion rightAccordion = new Accordion();
+	private final StackPane center = new StackPane();
+
+	private final BorderPane root = new BorderPane(center);
+	{
+		root.setRight(rightAccordion);
+		root.setLeft(leftMenu);
+	}
+
 	@Override
 	public Stage display(Stage stage, ApplicationProperties properties) {
-		// TODO Auto-generated method stub
-		return null;
+		stage.setScene(new Scene(root));
+		return stage;
 	}
 
 }
