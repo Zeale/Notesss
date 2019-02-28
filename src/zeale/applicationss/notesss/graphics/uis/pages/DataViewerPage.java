dcda287072e68f7ebf7c679af40592fdf5e4918a
Zeale <zeale@dusttoash.org>
2019-02-28 12:17:34 -0600

    Updated data viewer page

--- src/zeale/applicationss/notesss/graphics/uis/pages/DataViewerPage.java
+++ src/zeale/applicationss/notesss/graphics/uis/pages/DataViewerPage.java
@@ -1,14 +1,22 @@
 package zeale.applicationss.notesss.graphics.uis.pages;
 
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 import zeale.applicationss.notesss.ApplicationProperties;
 
 public class DataViewerPage implements Page {
+	
+	private final TilePane layout = new TilePane();
+	private final AnchorPane center = new AnchorPane(layout);
+	private final BorderPane root = new BorderPane(center);
 
 	@Override
 	public Stage display(Stage stage, ApplicationProperties properties) {
-		// TODO Auto-generated method stub
-		return null;
+		stage.setScene(new Scene(root));
+		return stage;
 	}
 
 }
